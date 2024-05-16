59a37a57b6143ed01d6b8539c9914c45af84f986
TTrunDDun <116082792+TTrunDDun@users.noreply.github.com>
2024-05-16 16:06:55 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// Copyright 2022 UNN-IASR
+#include "automata.h"
+
+int main() {
+    automata d;
+
+    d.on();
+    d.etmenu();
+    d.coin(10);
+    d.coin(20);
+    d.getstate();
+    d.choice();
+    d.check();
+    d.cook();
+    d.finish();
+
+    return 0;
+}
